8cf4fc98152d71141b3bea31848390965eb9c687
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2020-09-28 09:34:30 -0500

    created warmup files

---
+++ JS/warmup.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log("hello harry");
+
+console.log("hello ron");
+
+console.log("hello hermione");
+
+console.log("hello neville");
+
+console.log("hello hagrid");
+
+function sayHello(name) {
+    console.log("hello" + name);
+}
+
+sayHello(name:"harry");
+sayHello(name:"ron");
+sayHello(name:"hermione");
+sayHello(name:"neville");
+sayHello(name:"hagrid");
+
+//make sure to use quotes. when you create a function like this, intellij will
