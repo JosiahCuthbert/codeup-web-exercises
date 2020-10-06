f1b80fcdf46fdd30d6e8d7ddbd379c2597bbfc06
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2020-10-06 14:02:18 -0500

    planets exercises complete

--- JS/planets-string.js
+++ JS/planets-string.js
@@ -28,6 +28,17 @@
     var planetsBreakString = planetsArray.join("\n");
     console.log(planetsBreakString);
 
-    var planetsList = ()
+    function makeUL(array) {
+        var list = document.createElement('ul');
+        for (var i = 0; i < array.length; i++) {
+            var item = document.createElement('li');
+            item.appendChild(document.createTextNode(array[i]));
+            list.appendChild(item);
+        }
+        return list;
+    }
+
+    console.log(makeUL(planetsArray));
+
 })();
 
