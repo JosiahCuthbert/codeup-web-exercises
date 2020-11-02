dcde8dfc8feb333a8f8494a692745ad1cfd6bc8e
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2020-11-02 14:07:20 -0600

    First BOM exercise in progress

--- JS/warmup.js
+++ JS/warmup.js
@@ -1,21 +1,14 @@
-"use strict";
+var vowels=["a","e","i","o","u","A","E","I","O","U"]
 
-console.log("hello dumbledore")
-console.log("hello harry");
-console.log("hello ron");
-console.log("hello hermione");
-console.log("hello neville");
-console.log("hello hagrid");
-function sayHello(name) {
-    console.log("hello" + name);
+function hasVowels(str) {
+    for (var i = 0; i <= str.length; i++) {
+        if (str.indexOf(vowels[i]) == -1) {
+            return false;
+        }
+    }
+    return true;
 }
 
-sayHello("dumbledore")
-sayHello(name:"harry");
-sayHello(name:"ron");
-sayHello(name:"hermione");
-sayHello(name:"neville");
-sayHello(name:"hagrid");
 
 //make sure to use quotes. when you create a function like this, intellij will autofill your theme (in this case "name:")
 //if you only want to add a single file, use git add filename. if you do a git status then begin your git add and begin typing the file name, "tab" will autocomplete
