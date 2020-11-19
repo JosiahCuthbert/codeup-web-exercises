a55031c22d3f64b62d13a45794e6deee7ab76eb3
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2020-11-19 09:01:32 -0600

    weather map in progress

--- JS/weather_map.js
+++ JS/weather_map.js
@@ -13,90 +13,63 @@
         lon: -98.48527,
         units: "imperial",
         exclude: "minutely,hourly,alerts"
-    }).done(function(data){
-        console.log(data);
+    }).done(function(){
+        enablePins();
+        console.log(currentMarker);
     });
 
-    var currentMarkers = [];
+    var currentMarker = null;
+
+    var currentDate = new Date();
+
+    var daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
     function dropPin(coordinates){
-        var marker = new mapboxgl.Marker()
-        currentMarkers.push(marker);
-        currentMarkers[0].setLngLat(coordinates)
-        currentMarkers[0].addTo(map);
+        if(currentMarker == null){
+        var marker = new mapboxgl.Marker();
+        currentMarker = marker;
+        }
+        currentMarker.setLngLat(coordinates)
+        currentMarker.addTo(map);
     }
 
     function enablePins() {
         map.on('click', function (e) {
-            dropPin(e.lngLat.wrap());
+            dropPin(e.lngLat);
             console.log(e.lngLat);
-            // retrieveWeather(e.lngLat.lng, e.lngLat.lat);
+            retrieveWeather(e.lngLat.lng, e.lngLat.lat);
         });
     }
 
-    // function retrieveWeather(longitude, latitude){
-    //     $.get("https://api.openweathermap.org/data/2.5/onecall", {
-    //         APPID: weatherMapAPIKey,
-    //         lat: latitude,
-    //         lon: longitude,
-    //         units: "imperial",
-    //         exclude: "minutely,hourly,alerts"
-    //     }).done(function(data){
-    //         console.log(data);
-    //     });
-    // }
+    function retrieveWeather(longitude, latitude){
+        $.ajax("https://api.openweathermap.org/data/2.5/onecall", {
+            data: {
+                APPID: weatherMapAPIKey,
+                lat: latitude,
+                lon: longitude,
+                units: "imperial",
+                exclude: "minutely,hourly,alerts"
+            }
+        }).done(function(data, status, jqXhr){
+            console.log(data);
+            fiveDayForecast(data);
+        });
+    }
+
+    function fiveDayForecast(data){
+        for(var i = 0;i<5;i++){
+            var day = (currentDate.getDay() + i) % (daysOfTheWeek.length);
+            console.log(data.daily[i].weather[0].main);
+            console.log(day);
+            $(".forecast-day:eq(0)").css("background-color", "red")
+        }
+    }
+
+
 
 
 //create an event listener that places a pin when a user clicks the map and logs the coordinates
 //Then take those coordinates and plug them in to get info back from thw weather map API
 //Request and log the info you need from the API and then eventually show that info in the UI
-
-
-
-
-
-
-
-
-
-// mapboxgl.accessToken = newMapboxToken;
-    // var map = new mapboxgl.Map({
-    //     container: 'map',
-    //     style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
-    //     center: [-98.4916, 29.4252], // starting position [lng, lat]
-    //     zoom: 4 // starting zoom
-    // });
-
-    // var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-
-
-    // var favoriteRestaurants =[
-    //     {
-    //         name: "oui Banh Mi",
-    //         address: "1601 Richmond Ave Houston, Texas 77006"
-    //     },
-    //     {
-    //         name: "Cooking Girl",
-    //         address: "2400 W Holcombe Blvd Houston, TX 77030"
-    //     },
-    //     {
-    //         name: "Two Guys Pizzeria",
-    //         address: "2250 W Holcombe Blvd Houston, TX 77030"
-    //     }]
-    //
-    // favoriteRestaurants.forEach(function(restaurant){
-    //     geocode(restaurant.address, mapboxToken).then(function(coordinates){
-    //         var marker = new mapboxgl.Marker()
-    //             .setLngLat(coordinates)
-    //             .addTo(map);
-    //         var popUp = new mapboxgl.Popup()
-    //             .setHTML(restaurant.name)
-    //             .addTo(map)
-    //
-    //         marker.setPopup(popUp)
-    //     })
-    // })
-
-
 
 // })();
